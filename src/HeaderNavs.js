0523360a64a11d66b0a72a2c9cd760e7966dc5cc
Maniz <mashres15@earlham.edu>
2017-06-13 14:58:43 -0400

    Separated the header, aside, article, footer

---
+++ src/HeaderNavs.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+//import './HeaderNavs.css'
+
+class HeaderNavs extends Component {
+  render() {
+    return (
+      <div className="float-right nav-links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Log in</a>
+      </div>
+    )
+  }
+}
+
+export default HeaderNavs
---
+++ src/HeaderTitle.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+//import './HeaderTitle.css'
+
+class HeaderTitle extends Component {
+    render() {
+        return (
+            <div className="headline text-center small-12 columns">
+              <h1>The Dwarf Underground</h1>
+              <h3>Delving deep for the <span className="gold">shiniest</span> news</h3>
+            </div>
+        )
+    }
+}
+
+export default HeaderTitle
